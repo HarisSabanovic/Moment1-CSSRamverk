8544cfb20be0c3876471eda3b3259ebbf9497597
Haris Sabanovic <hasa2303@student.miun.se>
2024-11-20 15:17:30 +0100

    first commit

---
+++ js/bulma.js
@@ -0,0 +1,86 @@
+const formEL = document.getElementById("formID");
+const tableEl = document.getElementById("tableID");
+
+document.addEventListener('DOMContentLoaded', () => {
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    if ($navbarBurgers.length > 0) {
+        $navbarBurgers.forEach(el => {
+            el.addEventListener('click', () => {
+                const target = el.dataset.target;
+                const $target = document.getElementById(target);
+                el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+            });
+        });
+    }
+});
+
+
+
+loadData();
+
+formEL.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+
+    let nameInput = document.getElementById("firstname").value;
+    let countryInput = document.getElementById("country").value;
+    let genderInput = document.querySelector('input[name="gender"]:checked').value;
+    let infoInput = document.getElementById("info").value;
+
+    let newPerson = document.createElement("tr");
+
+    
+    newPerson.innerHTML = `
+        <td>${nameInput}</td>
+        <td>${genderInput}</td>
+        <td>${countryInput}</td>
+        <td>${infoInput}</td>
+    `;
+
+    tableEl.querySelector("tbody").appendChild(newPerson);
+
+    saveTableData();
+
+});
+
+function saveTableData() {
+    let rows = tableEl.querySelectorAll("tbody tr");
+    let bulmaData = [];
+
+    rows.forEach(row => {
+        let person = {
+            name: row.cells[0].innerHTML,
+            gender: row.cells[1].innerHTML,
+            country: row.cells[2].innerHTML,
+            info: row.cells[3].innerHTML
+        }
+
+        bulmaData.push(person);
+    })
+
+    localStorage.setItem("bulmaData", JSON.stringify(bulmaData));
+
+    loadData();
+}
+
+
+function loadData() {
+    tableEl.querySelector("tbody").innerHTML = "";
+    let bulmaData = JSON.parse(localStorage.getItem("bulmaData"));
+
+    if (bulmaData) {
+        bulmaData.forEach(person => {
+            let newPerson = document.createElement("tr");
+
+            newPerson.innerHTML = `
+                <td>${person.name}</td>
+                <td>${person.gender}</td>
+                <td>${person.country}</td>
+                <td>${person.info}</td>
+                `;
+
+                tableEl.querySelector("tbody").appendChild(newPerson);
+        })
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,70 @@
+
+const formEL = document.getElementById("formID");
+const tableEl = document.getElementById("tableID");
+
+document.addEventListener("DOMContentLoaded", loadData);
+
+formEL.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let nameInput = document.getElementById("firstname").value;
+    let countryInput = document.getElementById("country").value;
+    let genderInput = document.querySelector('input[name="gender"]:checked').value;
+    let infoInput = document.getElementById("info").value;
+
+    let newPerson = document.createElement("tr");
+
+    
+    newPerson.innerHTML = `
+        <td>${nameInput}</td>
+        <td>${genderInput}</td>
+        <td>${countryInput}</td>
+        <td>${infoInput}</td>
+    `;
+
+    tableEl.querySelector("tbody").appendChild(newPerson);
+
+    saveTableData();
+
+});
+
+function saveTableData() {
+    let rows = tableEl.querySelectorAll("tbody tr");
+    let tableData = [];
+
+    rows.forEach(row => {
+        let person = {
+            name: row.cells[0].innerHTML,
+            gender: row.cells[1].innerHTML,
+            country: row.cells[2].innerHTML,
+            info: row.cells[3].innerHTML
+        }
+
+        tableData.push(person);
+    })
+
+    localStorage.setItem("tableData", JSON.stringify(tableData));
+
+    loadData();
+}
+
+
+function loadData() {
+    tableEl.querySelector("tbody").innerHTML = "";
+    let tableData = JSON.parse(localStorage.getItem("tableData"));
+
+    if (tableData) {
+        tableData.forEach(person => {
+            let newPerson = document.createElement("tr");
+
+            newPerson.innerHTML = `
+                <td>${person.name}</td>
+                <td>${person.gender}</td>
+                <td>${person.country}</td>
+                <td>${person.info}</td>
+                `;
+
+                tableEl.querySelector("tbody").appendChild(newPerson);
+        }
+    )};
+}
